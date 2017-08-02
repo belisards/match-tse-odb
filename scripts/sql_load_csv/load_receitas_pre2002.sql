090f833898590a1c61eacc966c40735f21894a36
adriano <adrianobf@gmail.com>
2017-08-02 12:34:19 -0300

    inserir script para baixar pre2002

---
+++ scripts/sql_load_csv/load_receitas_pre2002.sql
@@ -0,0 +1,23 @@
+use tse
+
+# ELECTION YEAR,FUNCTION,STATE,CAND NAME,CAND BALLOT NAME,CAND FISCAL IDENTIFICATION,PARTY,DATE OF DONATION,DONOR NAME,VALUE IN R$,VALUE IN US$,DONOR CLASSIFICATION,TIPO DE DOADOR,ECONOMIC ACTIVITY,SETOR ECONÔMICO,REASON TO CORRECT,,
+  
+-- load candidatos
+load data local infile '/home/abitporu/Connectas/tribuna/pre2002.csv'
+  into table doacoes
+    fields terminated by ','
+    lines terminated by '\n'
+    ignore 1 lines
+    (@ELECTION_YEAR, @FUNCTION, @STATE, @CAND_NAME, @CAND_BNAME, @CAND_ID, @PARTY, @DATE_DONATION, @DONOR_NAME, @VALOR,@VALUE,DONOR_CLAS, @TIPO_DE_DOADOR, @ECONOMIC_ACTIVITY, @SETOR_ECONÔMICO, @REASON_TO_CORRECT,@NA,)
+SET
+  ano=TRIM(@ELECTION_YEAR),
+  tipo="candidato",
+  uf=TRIM(@STATE),
+  candidato=TRIM(@CAND_NAME),
+  cargo=TRIM(@FUNCTION),
+  doador=TRIM(@DONOR_NAME),
+  partido=TRIM(@PARTY),
+  recurso=TRIM(@tipo),
+  data=LEFT(TRIM(@DATE_DONATION), 10),
+  valor=IF(TRIM(@VALOR) = '', NULL, REPLACE(TRIM(@VALOR), ',', '.'));
+SHOW WARNINGS;
---
+++ scripts/sql_load_csv/match.sql
@@ -0,0 +1,247 @@
+USE tse;
+# Script para importar e fazer o match do Grupo Odebrecht
+
+DROP TABLE IF EXISTS odb;
+
+CREATE TABLE odb (
+    nome varchar(255),
+    documento varchar(20),
+    tipo varchar(255)
+);
+
+load data local infile 'csv/match.csv' into table odb CHARACTER SET UTF8 fields terminated by ','  enclosed by '"'  lines terminated by '\n'  (nome, documento, tipo);
+
+# Crias as views
+CREATE OR REPLACE VIEW documentos
+AS
+SELECT DISTINCT(TRIM(documento)) AS documento
+FROM odb
+WHERE TRIM(documento) <> "";
+
+CREATE OR REPLACE VIEW nomes
+AS
+SELECT DISTINCT(TRIM(nome)) AS nome
+FROM odb
+WHERE TRIM(nome) <> "";
+
+
+CREATE OR REPLACE VIEW prefixos_cnpj
+AS
+SELECT DISTINCT SUBSTR(TRIM(documento), 1, 8) AS inicio
+FROM odb
+WHERE TRIM(documento) <> "" AND LENGTH(documento) > 11;
+
+# Cria tabela por filial - CNPJ do doador originario
+DROP TABLE IF EXISTS doa_filial_orig;
+CREATE TABLE doa_filial_orig AS
+SELECT 
+doacoes.id,
+doacoes.uf,
+doacoes.partido,
+doacoes.cargo,
+doacoes.candidato,
+doacoes.numero,
+doacoes.ano,
+doacoes.cpf_candidato,
+doacoes.doador,
+doacoes.doador_original,
+doacoes.cpf_doador,
+doacoes.cpf_doador_original,
+doacoes.recurso,
+doacoes.data,
+doacoes.setor_economico,
+doacoes.motivo,
+doacoes.valor,
+doacoes.tipo
+FROM doacoes INNER JOIN prefixos_cnpj 
+ON TRIM(doacoes.cpf_doador_original) LIKE CONCAT(prefixos_cnpj.inicio, '%')
+WHERE LENGTH(TRIM(doacoes.cpf_doador_original)) > 11 ;
+
+# Cria tabela por filial
+DROP TABLE IF EXISTS doa_filial;
+CREATE TABLE doa_filial AS
+SELECT 
+doacoes.id,
+doacoes.uf,
+doacoes.partido,
+doacoes.cargo,
+doacoes.candidato,
+doacoes.numero,
+doacoes.ano,
+doacoes.cpf_candidato,
+doacoes.doador,
+doacoes.doador_original,
+doacoes.cpf_doador,
+doacoes.cpf_doador_original,
+doacoes.recurso,
+doacoes.data,
+doacoes.setor_economico,
+doacoes.motivo,
+doacoes.valor,
+doacoes.tipo
+FROM doacoes INNER JOIN prefixos_cnpj 
+ON TRIM(doacoes.cpf_doador) LIKE CONCAT(prefixos_cnpj.inicio, '%')
+WHERE LENGTH(TRIM(doacoes.cpf_doador)) > 11 ;
+
+# Cria tabela por documento
+DROP TABLE IF EXISTS doa_doc;
+CREATE TABLE doa_doc AS
+SELECT 
+doacoes.id,
+doacoes.uf,
+doacoes.partido,
+doacoes.cargo,
+doacoes.candidato,
+doacoes.numero,
+doacoes.ano,
+doacoes.cpf_candidato,
+doacoes.doador,
+doacoes.doador_original,
+doacoes.cpf_doador,
+doacoes.cpf_doador_original,
+doacoes.recurso,
+doacoes.data,
+doacoes.setor_economico,
+doacoes.motivo,
+doacoes.valor,
+doacoes.tipo
+FROM doacoes
+INNER JOIN documentos
+ON TRIM(doacoes.cpf_doador) = documentos.documento;
+
+# Cria tabela por documento - CPF original
+DROP TABLE IF EXISTS doa_doc_orig;
+CREATE TABLE doa_doc_orig AS
+SELECT 
+doacoes.id,
+doacoes.uf,
+doacoes.partido,
+doacoes.cargo,
+doacoes.candidato,
+doacoes.numero,
+doacoes.ano,
+doacoes.cpf_candidato,
+doacoes.doador,
+doacoes.doador_original,
+doacoes.cpf_doador,
+doacoes.cpf_doador_original,
+doacoes.recurso,
+doacoes.data,
+doacoes.setor_economico,
+doacoes.motivo,
+doacoes.valor,
+doacoes.tipo
+FROM doacoes
+INNER JOIN documentos
+ON TRIM(doacoes.cpf_doador_original) = documentos.documento;
+
+# Cria tabela por LIKE
+DROP TABLE IF EXISTS doa_nomelike;
+CREATE TABLE doa_nomelike AS 
+SELECT 
+doacoes.id,
+doacoes.uf,
+doacoes.partido,
+doacoes.cargo,
+doacoes.candidato,
+doacoes.numero,
+doacoes.ano,
+doacoes.cpf_candidato,
+doacoes.doador,
+doacoes.doador_original,
+doacoes.cpf_doador,
+doacoes.cpf_doador_original,
+doacoes.recurso,
+doacoes.data,
+doacoes.setor_economico,
+doacoes.motivo,
+doacoes.valor,
+doacoes.tipo
+FROM doacoes
+WHERE doador LIKE '%Brask%' OR doador LIKE '%Odebre%' OR doador LIKE '%Ordebr%' 
+OR doador LIKE '%Brasque%' OR  doador LIKE '%Oubrecht%' OR doador LIKE '%Adebrecht%' OR doador LIKE '%Odevrecht%' OR doador LIKE '%Oderbrecht%' OR doador LIKE '%Edebrecht%' OR doador LIKE '%Construtora Noberto%' OR doador LIKE 'Oubrecht'
+OR doador_original LIKE '%Brask%' OR doador_original LIKE '%Odebre%' 
+OR doador_original LIKE '%Brasque%' OR  doador_original LIKE '%Oubrecht%' OR doador_original LIKE '%Adebrecht%' OR doador_original LIKE '%Odevrecht%' OR doador_original LIKE '%Oderbrecht%' OR doador_original LIKE '%Edebrecht%' OR doador_original LIKE '%Construtora Noberto%' OR doador_original LIKE 'Oubrecht' ;
+
+# Consulta por nome
+DROP TABLE IF EXISTS doa_rzsocial;
+CREATE TABLE doa_rzsocial AS
+SELECT 
+doacoes.id,
+doacoes.uf,
+doacoes.partido,
+doacoes.cargo,
+doacoes.candidato,
+doacoes.numero,
+doacoes.ano,
+doacoes.cpf_candidato,
+doacoes.doador,
+doacoes.doador_original,
+doacoes.cpf_doador,
+doacoes.cpf_doador_original,
+doacoes.recurso,
+doacoes.data,
+doacoes.setor_economico,
+doacoes.motivo,
+doacoes.valor,
+doacoes.tipo
+FROM doacoes
+INNER JOIN nomes
+ON TRIM(doacoes.doador) = nomes.nome;
+
+# Consulta por nome - doador original
+DROP TABLE IF EXISTS doa_rzsocial_orig;
+CREATE TABLE doa_rzsocial_orig AS
+SELECT 
+doacoes.id,
+doacoes.uf,
+doacoes.partido,
+doacoes.cargo,
+doacoes.candidato,
+doacoes.numero,
+doacoes.ano,
+doacoes.cpf_candidato,
+doacoes.doador,
+doacoes.doador_original,
+doacoes.cpf_doador,
+doacoes.cpf_doador_original,
+doacoes.recurso,
+doacoes.data,
+doacoes.setor_economico,
+doacoes.motivo,
+doacoes.valor,
+doacoes.tipo
+FROM doacoes
+INNER JOIN nomes
+ON TRIM(doacoes.doador_original) = nomes.nome;
+
+# CRIA TABELA FINAL
+DROP TABLE IF EXISTS grupodb;
+CREATE TABLE grupodb AS
+SELECT 
+*
+FROM doa_rzsocial
+UNION
+SELECT 
+*
+FROM doa_nomelike
+UNION
+SELECT 
+*
+FROM doa_doc
+UNION
+SELECT 
+*
+FROM doa_filial
+UNION
+SELECT 
+*
+FROM doa_rzsocial_orig
+UNION
+SELECT 
+*
+FROM doa_filial_orig
+UNION
+SELECT 
+*
+FROM doa_doc_orig;
