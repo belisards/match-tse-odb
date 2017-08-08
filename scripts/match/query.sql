d453e73b13820a6e63a6ff0b94a1934b3b4c628e
adriano <adrianobf@gmail.com>
2017-08-07 23:11:44 -0300

    reglariza e ajusta

--- scripts/match/query.sql
+++ scripts/match/query.sql
@@ -1,80 +1,5 @@
 USE tse;
 
-
-# Regulariza nomes
-UPDATE grupodb
-SET doador_norm = 'Construtora'
-WHERE cpf_doador LIKE '15102288%' 
-or doador LIKE 'NORBERTO ODEBRECHT S.A%'
-or (doador not like 'CETREL%' and doador like 'C%'  );
-
-UPDATE grupodb
-SET doador_norm = 'Braskem'
-WHERE cpf_doador LIKE '42150391%'
-or doador like 'Braske%'
-or doador like 'BRASKE%';
-
-UPDATE grupodb
-SET doador_norm = 'Quattor'
-WHERE doador LIKE 'Quattor%' or doador LIKE 'Quattar%';
-
-UPDATE grupodb
-SET doador_norm = 'O. Realizações Imobiliárias'
-WHERE doador LIKE 'Odebrecht Reali%'
-or doador LIKE 'ODEBRECHT REAL%';
-
-UPDATE grupodb
-SET doador_norm = 'O. Agroindustrial'
-WHERE doador LIKE 'Odebrecht Agro%'
-or doador LIKE 'ODEBRECHT AGROI%';
-
-UPDATE grupodb
-SET doador_norm = 'O. Ambiental'
-WHERE doador LIKE 'ODEBRECHT AMBIENTAL%';
-
-UPDATE grupodb
-SET doador_norm = 'Pedro Carneiro Leão'
-WHERE doador LIKE 'PEDRO AUGUS%';
-
-UPDATE grupodb
-SET doador_norm = 'Alexandrino Alencar'
-WHERE doador LIKE 'Alexandrino de%';
-
-UPDATE grupodb
-SET doador_norm = 'Embraport'
-WHERE doador LIKE 'EMBRAP%';
-
-UPDATE grupodb
-SET doador_norm = 'Enseada Ind. Naval'
-WHERE doador LIKE 'ENSEADA%' or doador LIKE 'ESTALEIRO%';
-
-UPDATE grupodb
-SET doador_norm = 'ETH'
-WHERE doador LIKE 'ETH%';
-
-UPDATE grupodb
-SET doador_norm = 'Ismael Campos'
-WHERE doador LIKE 'Ismael Campos%';
-
-UPDATE grupodb
-SET doador_norm = 'Ruy Lemos'
-WHERE doador LIKE 'RUI LEMOS%' or doador LIKE 'RUY LEMOS%';
-
-UPDATE grupodb
-SET doador_norm = 'O. Óleo e Gás' 
-WHERE doador LIKE 'ODEBRECHT OLEO%' or doador LIKE 'ODERBRECHT OLE%' or doador LIKE 'ODEBRECH OLEO%' or doador LIKE 'ODEBRECHT ÓLEO%';
-
-UPDATE grupodb
-SET partido = 'DEM' 
-WHERE partido = ' PFL';
-
-# Copia o campo doador original para o campo doador, quando existir
-update grupodb
-SET doador_norm = doador_original
-where doador_original is not null and doador_original <> '';
-
-UPDATE grupodb SET `doador_norm` = UPPER( `doador` );
-
 # Geral
 SELECT 
 grupodb.id,
@@ -104,26 +29,10 @@ FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n';
 
-SELECT doador 
-FROM grupodb
-GROUP by doador
-INTO OUTFILE '/var/lib/mysql-files/grupo.csv'
-FIELDS TERMINATED BY ','
-ENCLOSED BY '"'
-LINES TERMINATED BY '\n';
 
-
-SELECT id, uf, partido, cargo, candidato, ano, doador, dolar 
+SELECT id, uf, partido, cargo, candidato, ano, doador_norm, dolar 
 FROM grupodb
 INTO OUTFILE '/var/lib/mysql-files/table-dolar.csv'
-FIELDS TERMINATED BY ','
-ENCLOSED BY '"'
-LINES TERMINATED BY '\n';
-
-/*
-# Total por doador
-select distinct candidato from grupodb where candidato not like '%financeiro%' and candidato not like 'Direção%' group by candidato
-INTO OUTFILE '/var/lib/mysql-files/doador.csv'
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n';
---
+++ scripts/match/regulariza.sql
@@ -0,0 +1,166 @@
+USE tse;
+
+update grupodb
+SET doador_norm = doador;
+
+# Copia o campo doador original para o campo doador, quando existir
+update grupodb
+SET doador_norm = doador_original
+where doador_original is not null and doador_original <> '';
+
+
+# Regulariza nomes
+UPDATE grupodb
+SET doador_norm = 'Construtora'
+WHERE cpf_doador LIKE '15102288%' 
+or doador LIKE 'Cons%'
+or (doador = 'NORBERTO ODEBRECHT S.A' and ano = '1994')
+or doador LIKE 'CO0NS%'
+or doador LIKE 'C N ODEB%'
+or doador LIKE 'Contrutora%'
+or doador_original LIKE 'Const%'
+or id = '70970';
+
+UPDATE grupodb
+SET doador_norm = 'NORBERTO ODEBRECHT'
+WHERE doador LIKE 'NORBERTO ODEBRECHT';
+
+UPDATE grupodb
+SET doador_norm = 'Braskem'
+WHERE cpf_doador LIKE '42150391%'
+or doador like 'Braske%'
+or doador_original like 'BRASKE%';
+
+UPDATE grupodb
+SET doador_norm = 'Quattor'
+WHERE doador LIKE 'Quattor%' or doador LIKE 'Quattar%';
+
+UPDATE grupodb
+SET doador_norm = 'Realizações Imobiliárias'
+WHERE doador LIKE 'Odebrecht Reali%'
+or doador_original LIKE 'ODEBRECHT REAL%';
+
+UPDATE grupodb
+SET doador_norm = 'Agroindustrial'
+WHERE doador LIKE 'Odebrecht Agro%'
+or doador_original LIKE 'ODEBRECHT AGROI%';
+
+UPDATE grupodb
+SET doador_norm = 'Ambiental'
+WHERE doador LIKE 'ODEBRECHT AMBIENTAL%' or doador_original LIKE 'ODEBRECHT AMBIENTAL%';
+
+UPDATE grupodb
+SET doador_norm = 'Serviços'
+WHERE doador LIKE 'ODEBRECHT SERV%' or doador_original LIKE 'ODEBRECHT SERV%';
+
+UPDATE grupodb
+SET doador_norm = 'ODBINV'
+WHERE cpf_doador LIKE '1510558800%';
+
+UPDATE grupodb
+SET doador_norm = 'Pedro Carneiro Leão'
+WHERE doador LIKE 'PEDRO AUGUS%';
+
+UPDATE grupodb
+SET doador_norm = 'Alexandrino Alencar'
+WHERE doador LIKE 'Alexandrino de%';
+
+UPDATE grupodb
+SET doador_norm = 'ANTONIO DE CASTRO'
+WHERE doador LIKE 'ANTONIO DE CASTRO%';
+
+UPDATE grupodb
+SET doador_norm = 'Embraport'
+WHERE doador LIKE 'EMBRAP%'
+or doador_original LIKE 'EMBRAP%';
+
+UPDATE grupodb
+SET doador_norm = 'Enseada Ind. Naval'
+WHERE doador LIKE 'ENSEADA%' or doador LIKE 'ESTALEIRO%'
+OR doador_original LIKE 'ENSEADA%' or doador_original LIKE 'ESTALEIRO%';
+
+UPDATE grupodb
+SET doador_norm = 'ETH'
+WHERE doador LIKE 'ETH%';
+
+UPDATE grupodb
+SET doador_norm = 'Ismael Campos'
+WHERE doador LIKE 'Ismael Campos%';
+
+UPDATE grupodb
+SET doador_norm = 'Arnaldo Cumplido'
+WHERE doador LIKE 'Arnaldo Cumplido%';
+
+UPDATE grupodb
+SET doador_norm = 'FABIO GANDOLFO'
+WHERE doador LIKE 'FABIO ANDREANI%';
+
+UPDATE grupodb
+SET doador_norm = 'FRANCISCO PELTIER'
+WHERE doador LIKE 'FRANCISCO PELTIER%';
+
+UPDATE grupodb
+SET doador_norm = 'Emílio O. Peltier'
+WHERE doador LIKE 'EMILIO ODEBRECHT P%';
+
+UPDATE grupodb
+SET doador_norm = 'Hilberto Mascarenhas'
+WHERE doador LIKE 'Hilberto Mascarenhas%';
+
+UPDATE grupodb
+SET doador_norm = 'Ilka O. Peltier'
+WHERE doador LIKE 'Ilka O%';
+
+UPDATE grupodb
+SET doador_norm = 'João Gouveia'
+WHERE doador LIKE 'JOAO G%';
+
+UPDATE grupodb
+SET doador_norm = 'Luiz Schneider'
+WHERE doador LIKE 'LUIZ ANTONIO%'
+or doador_original LIKE 'LUIZ ANTONIO%';
+
+UPDATE grupodb
+SET doador_norm = 'VICTOR GRADIN'
+WHERE doador LIKE 'VICTOR GRADIN%';
+
+UPDATE grupodb
+SET doador_norm = 'PAULO BARQUEIRO'
+WHERE doador LIKE 'PAULO RICARDO%'
+or doador_original LIKE 'PAULO RICARDO%';
+
+UPDATE grupodb
+SET doador_norm = 'SERGIO LUIZ'
+WHERE doador LIKE 'SERGIO LUIZ%'
+or doador_original LIKE 'SERGIO LUIZ%';
+
+
+UPDATE grupodb
+SET doador_norm = 'RICARDO ROTH'
+WHERE doador LIKE 'RICARDO ROTH%'
+or doador_original LIKE 'RICARDO ROTH%';
+
+UPDATE grupodb
+SET doador_norm = 'PEDRO CARNEIRO'
+WHERE doador LIKE 'PEDRO CARNEIRO%'
+or doador LIKE 'PEDRO AUGUSTO CARNEIRO%'
+or doador_original LIKE 'PEDRO CARNEIRO%';
+
+UPDATE grupodb
+SET doador_norm = 'Ruy Lemos'
+WHERE doador LIKE 'RUI LEMOS%' or doador LIKE 'RUY LEMOS%';
+
+UPDATE grupodb
+SET doador_norm = 'Óleo e Gás' 
+WHERE doador LIKE 'ODEBRECHT O%' or doador LIKE 'ODEBRECHT Ó%' or doador LIKE 'ODERBRECHT OL%' or doador LIKE 'ODEBRECH O%' or doador LIKE 'ODEBRECH Ó%' or doador_original LIKE 'ODEBRECHT O%' or doador_original LIKE 'ODEBRECHT Ó%';
+
+UPDATE grupodb
+SET doador_norm = 'CETREL' 
+WHERE doador LIKE 'CETREL%'
+or doador_original LIKE 'CETREL%';
+
+UPDATE grupodb
+SET partido = 'DEM' 
+WHERE partido = 'PFL';
+
+UPDATE grupodb SET `doador_norm` = UPPER( `doador_norm` );
