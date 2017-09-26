ee0cb9fe81f70b8bd1495537bc6b29365305aeba
adriano <adrianobf@gmail.com>
2017-09-26 12:31:06 -0300

    atualiza query

--- queries.sql
+++ queries.sql
@@ -1,89 +1,93 @@
 
 ############################
 ## 
+
 ## CONNECTAS
 ##
 ############################
+
 
 # Total
 select sum(dolar) from o;
 select sum(valor_at) from o;
 
-# Total of companies
+# Total de empresas
 select cpf_doador_original  from grupodb where length(cpf_doador_original ) > '11' and  (cpf_doador_original <> '' or cpf_doador_original IS NOT NULL) group by cpf_doador_original;
 
-# Total of politicians 
+# Total de políticos 
 select cpf_candidato from grupodb where ano > '2000' and tipo = 'candidato' and length(cpf_candidato) < '12' and cpf_candidato <> '' group by cpf_candidato;
 
-# Total of entries in TSE dataset
+# Total de entradas no TSE
 select count(id) from doacoes where ano > '2000';
 
-# Total of people and companies
+# Total de pessoas e empresas
 select doador_norm from grupodb where ano >  '2000' group by doador_norm ;
 select doador_norm, sum(valor_at) as valor from grupodb where ano >  '2000' group by doador_norm  order by valor desc;
 
-
-# Total from parties (including closed ones)
-select partido from grupodb group by partido;
-
-# Amount by party
+# Total por partido
 select partido, sum(dolar) as valor from o group by partido order by valor desc;
+select partido, sum(valor_at) as valor from grupodb where ano = '2014' group by partido order by valor desc;
+select partido, sum(valor_at) as valor from grupodb where ano = '2014' and tipo <> 'Candidato' group by partido order by valor desc;
 
-# Total of entries of Odebrecht dataset;
+# N. total de doação da Odb
 select count(id) from grupodb where ano > '2000';
 
-# Amount Braskem + Construtora
+# Total Braskem + Construtora
  select sum(dolar) as valor from o where doador_norm like 'const%' or doador_norm like 'bras%';
 
- # Amount by type
+ # Total por tipo
  select tipo, sum(dolar) from o group by tipo;
  select tipo, sum(valor_at) from o group by tipo;
 
- # Amount by payment type
+ # Total por forma de pagamento
 select recurso, sum(dolar) as d from o group by recurso order by d desc;
 select recurso, sum(valor_at) as d from o group by recurso order by d desc;
 
-# Total Odebrecht companies
-select cpf_doador from grupodb where length(cpf_doador) > '11' and  (cpf_doador_original = '' or cpf_doador_original IS NULL) group by cpf_doador
+# Total Odebrecht 
+select cpf_doador from grupodb where length(cpf_doador) > '11' and  (cpf_doador_original = '' or cpf_doador_original IS NULL) group by cpf_doador;
 
 select cpf_doador_original  from grupodb where length(cpf_doador_original ) > '11' and  (cpf_doador_original <> '' or cpf_doador_original IS NOT NULL) group by cpf_doador_original ;
 
 select cpf_doador  from grupodb where length(cpf_doador_original ) > '11' and  (cpf_doador_original <> '' or cpf_doador_original IS NOT NULL) group by cpf_doador;
 
- select doador_original , cpf_doador_original  from grupodb where length(cpf_doador_original ) > '11' and  (cpf_doador_original <> '' or cpf_doador_original IS NOT NULL) group by doador_original , cpf_doador_original ;
+select doador_original , cpf_doador_original  from grupodb where length(cpf_doador_original ) > '11' and  (cpf_doador_original <> '' or cpf_doador_original IS NOT NULL) group by doador_original , cpf_doador_original ;
 
-# Amount by year
+# Total por ano
 select ano, sum(dolar) from o group by ano;
 
-# Campaing finance amount in 2014
-select sum(dolar) from doacoes where ano = '2014' and doador_original = '' and recurso NOT LIKE 'Estim%';
+# Total financiamento de campanha em 2014
+select sum(valor_at) from doacoes where ano = '2014' and doador_original = '' and recurso NOT LIKE 'Estim%';
 
-# Campaing finance from ODB - amount in 2014
+# Odebrecht 2014
 select sum(valor_at) from o where ano = '2014' and recurso NOT LIKE 'Estim%';
 
-# Amount by candidate
+# Total por candidatos
 select candidato, sum(dolar) as d from grupodb where ano > '2000' and tipo = 'candidato' group by candidato order by d desc limit 5;
+select candidato, sum(valor_at) as d from grupodb where ano > '2000' and tipo = 'candidato' group by candidato order by d desc limit 5;
 
 
-# Amount by party 2014
+# Total por partido 2014
  select partido, sum(dolar) as d from grupodb where ano = '2014' and tipo <> 'candidato' group by partido order by d desc limit 5;
 
-  select partido, sum(valor_at) as v from grupodb where ano = '2014' and tipo <> 'candidato' group by partido order by v desc limit 5;
+ select partido, sum(valor_at) as v from grupodb where ano = '2014' and tipo <> 'candidato' group by partido order by v desc limit 5;
 
- # Amount by UF
+ # Total por UF
  select uf, sum(dolar) as d from o group by uf order by d desc limit 5;
 
- # Funding for Temer
- select * from grupodb where candidato like 'michel%';
+ # Total para o Temer
+select * from grupodb where candidato like '%TEMER LULIA%';
 
-# Amount by third-party companies
-select sum(valor_at) from laranjas where doador_original  = '' or doador_original IS NULL and ano > '2008';
+ # Total para o Marina
+select * from grupodb where candidato like '%OSMARINA MARINA%';
+
+# Total por laranjas
+select sum(valor_at) from laranjas where doador_original  = '' or doador_original IS NULL and ano > '2007';
 select sum(dolar) from laranjas where doador_original  = '' or doador_original IS NULL and ano > '2008';
 
-# Amount between 2008 and 2014
+# Total entre 2008 e 2014
 select sum(valor_at) from o where ano > '2007' and ano < '2015';
 select sum(dolar) from o where ano > '2007' and ano < '2015';
-
+select tipo, sum(valor) from o where ano > '2007' and ano < '2015' group by tipo;
 
 ############################
 ## 
